1ac1d3f5c7f67ebf4df26141ae5259578825732e
Ankur Sao <ankur.sao7@gmail.com>
2020-02-02 13:06:22 +0530

    Command design pattern

--- Command/main.cpp
+++ Command/main.cpp
@@ -7,9 +7,81 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+class command
+{
+public:
+    virtual void execute() =0;
+};
+
+class fan
+{
+public:
+    fan(){cout<<"fan ";}
+    void off(){
+        cout<< "off \n";
+    }
+    
+    void on(){
+        cout<<"on \n";
+    }
+    
+};
+
+class fanOn : public command
+{
+    fan *lFan;
+public:
+    fanOn(fan *aInFan)
+    {
+        lFan = aInFan;
+    }
+    void execute()
+    {
+        lFan->on();
+    }
+};
+
+class fanOff:public command
+{
+    fan* lFan;
+public:
+    fanOff(fan *aInFan)
+    {
+        lFan = aInFan;
+    }
+    void execute()
+    {
+        lFan->off();
+    }
+};
+
+
+class Remote
+{
+    command *slot;
+public:
+    void setCommand(command *aInCommand)
+    {
+        slot = aInCommand;
+    }
+    void buttonWasPressed()
+    {
+        slot->execute();
+    }
+};
+
+int main(int argc, const char * argv[])
+{
+    Remote *lRemote = new Remote;
+    fan *lFan = new fan();
+    
+    lRemote->setCommand(new fanOff(lFan));
+    lRemote->buttonWasPressed();
+    cout<<endl;
+    
     return 0;
 }
